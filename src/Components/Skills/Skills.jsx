4ca2c9df0b120a1db81d8e28b1c8ec737fd34fd9
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-21 22:28:06 -0500

    added styling for skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -2,7 +2,25 @@ import './skills.css'
 
 function Skills(){
     return (
-        <h1>Skills</h1>
+        <section className='skillSection'>
+            <h2 className='skillsTitle'>Technical Skills</h2>
+            <div className='skillsContainer'>
+                <div className='skillCard'>
+                    <h4>Front-end</h4>
+                    <p>ReactJS - CSS3 - Javascript - DOM Manipulation - Responsive Design - HTML - Flex Box - CSS Grid</p>
+                </div>
+
+                <div className='skillCard'>
+                    <h4>Back-end</h4>
+                    <p>MongoDB - NodeJS - Express - Mongoose - Django - Python - RESTful API - Fetch - SQL - PostgreSQL</p>
+                </div>
+
+                <div className='skillCard'>
+                    <h4>Other</h4>
+                    <p>Github - Git - Heroku - OOP - Netlify - C# - Unity - Whimsicle - ERD charts</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
