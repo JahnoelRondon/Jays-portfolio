4fda826eda38478481ed68d2b92bc0f0b3dfa9d5
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-30 10:53:13 -0500

    added some proj info for my projects and card styling

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,10 +3,25 @@ import './projects.css'
 function Projects(){
 
     let proj = [
-        {title: 'myth', desc: 'its the myth thing'},
-        {title: 'avatar', desc: 'blue things'},
-        {title: 'amazon', desc: 'but them thangs'},
-        {title: 'booo', desc: 'scary businesss'}
+
+        {
+            title: 'Gnome Home', 
+            desc: 'Gnome home is a place for us folk tale loving historians. Here you will find mythology throughout history from different pantheons and cultures and be able to review them. Fill your curiosity and Submit your interest. The project was built using Node.js/Express, Mongoose, MongoDB, GoogleoAuth, Passport, EJS templates, Javascript, CSS, HTML',
+            img:'https://i.imgur.com/O9SD9RJ.png',
+            deployLink: 'https://gnomehome.herokuapp.com/',
+            ghubLink: 'https://github.com/JahnoelRondon/Gnome-Home'
+
+        },
+
+        {
+            title: 'Creature Slayers', 
+            desc: 'Creature Slayers is a Turn Based Web Browser game where you hunt down creatures for a bounty.The project was built using Pure JavaScript, DOM manipulation, Events, HTML, and CSS.',
+            img:'https://raw.githubusercontent.com/JahnoelRondon/TurnBasedGame/main/imgs/CreatureSlayersTitle.png',
+            deployLink: 'https://youthful-goldwasser-877b0e.netlify.app/',
+            ghubLink: 'https://github.com/JahnoelRondon/TurnBasedGame'
+
+        },
+
     ]
 
     console.log(proj)
@@ -21,7 +36,9 @@ function Projects(){
                   proj.map((obj, idx) => (
                     <div className='projCard' key={idx} >
                         <h3>{obj.title}</h3>
+                        <img src={obj.img}/>
                         <p>{obj.desc}</p>
+                        {/* make links here and style them to look like buttons */}
                     </div>
                   ))
               }
