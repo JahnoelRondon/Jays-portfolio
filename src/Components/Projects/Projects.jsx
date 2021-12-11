8ce05a31cc7fcc167544f29c421d3c37e8669ef3
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-11 11:53:30 -0500

    added tech field in objects to seperate tech used and description

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,7 +6,8 @@ function Projects(){
         // add To do proj here
         {
             title: 'Marq', 
-            desc: 'Marq is a organizational/list-making application. It has many features to keep track of your time management as well as planning. There are many todo apps however Marq tackles the list making apps from a different angle and seperates the different kind of simple notes a person might make. With Marq you have the utilities right out of the box however you still have the flexibility to choose what you do and how you organize yourself. This app was made using Django, Python, Bootstrap, Javascript, HTML and CSS.',
+            desc: 'Marq is a organizational/list-making application. It has many features to keep track of your time management as well as planning. There are many todo apps however Marq tackles the list making apps from a different angle and seperates the different kind of simple notes a person might make. With Marq you have the utilities right out of the box however you still have the flexibility to choose what you do and how you organize yourself.',
+            tech: "Django, Python, Bootstrap, Javascript, HTML and CSS",
             img:'https://i.imgur.com/ryY4RLb.png',
             deployLink: 'https://p4marq.herokuapp.com/',
             ghubLink: 'https://github.com/JahnoelRondon/Marq-1'
@@ -16,6 +17,7 @@ function Projects(){
         {
             title: 'Travutan', 
             desc: 'Travutanrocketrocket is a web app that allows travelers to find everything they’re looking for about their next destination. Connecting travelers looking for help to find potential destinations, or simply browsing to know more about a specific destination.',
+            tech: "",
             img:'https://i.imgur.com/lvS01Bd.png',
             deployLink: null,
             ghubLink: 'https://github.com/lrmoon/travutan'
@@ -24,7 +26,8 @@ function Projects(){
 
         {
             title: 'Gnome Home', 
-            desc: 'Gnome home is a place for us folk tale loving historians. Here you will find mythology throughout history from different pantheons and cultures and be able to review them. Fill your curiosity and Submit your interest. The project was built using Node.js/Express, Mongoose, MongoDB, GoogleoAuth, Passport, EJS templates, Javascript, CSS, HTML',
+            desc: 'Gnome home is a place for us folk tale loving historians. Here you will find mythology throughout history from different pantheons and cultures and be able to review them. Fill your curiosity and Submit your interest.',
+            tech: "Node.js, Express, Mongoose, MongoDB, GoogleoAuth, Passport, EJS templates, Javascript, CSS, HTML",
             img:'https://i.imgur.com/hJf8Jjg.png?2',
             deployLink: 'https://gnomehome.herokuapp.com/',
             ghubLink: 'https://github.com/JahnoelRondon/Gnome-Home'
@@ -33,7 +36,8 @@ function Projects(){
 
         {
             title: 'Creature Slayers', 
-            desc: 'Creature Slayers is a Turn Based Web Browser game where you hunt down creatures for a bounty. The project was built using Pure JavaScript, DOM manipulation, Events, HTML, and CSS.',
+            desc: 'Creature Slayers is a Turn Based Web Browser game where you hunt down creatures for a bounty. The game was heavily inspired by hearthstone, wizard101, and magic the gathering.',
+            tech: "Javascript, DOM manipulation & event listeners, HTML, CSS",
             img:'https://i.imgur.com/ejxU7Jh.png',
             deployLink: 'https://youthful-goldwasser-877b0e.netlify.app/',
             ghubLink: 'https://github.com/JahnoelRondon/TurnBasedGame'
@@ -53,10 +57,12 @@ function Projects(){
                         <h3>{obj.title}</h3>
                         <img src={obj.img} alt='not valid'/>
                         <p>{obj.desc}</p>
-                        {/* make links here and style them to look like buttons */}
+                        
+                        <p><strong>Technologies used</strong>{obj.tech}</p>
+
                         <div className='projLinks'>
                             <a href={obj.ghubLink} alt='not valid' target="_blank" rel="noreferrer">View Repo</a>                            
-                            <a href={obj.deployLink ? obj.deployLink : "#"} alt='not valid' target="_blank" rel="noreferrer">{obj.deployLink ? "Live App" : "Under Construction"}</a>
+                            <a href={obj.deployLink ? obj.deployLink : ""} alt='not valid' target="_blank" rel="noreferrer">{obj.deployLink ? "Live App" : "Under Construction"}</a>
                         </div>
                     </div>
                   ))
