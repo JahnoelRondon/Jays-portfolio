9949409c34ebf651cdf77d5612e3adb03bf44f58
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-11 12:42:10 -0500

    addedd badge for banner on my status

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,7 +4,8 @@ function Banner(){
     return (
         <section className='bannerContainer'>
             <h1>Jahnoel Rondon</h1>    
-            <p>Full-Stack Developer</p>
+            <p className='position'>Full-Stack Developer</p>
+            <p className='badge'>Open to work!</p>
         </section>
     )
 }
