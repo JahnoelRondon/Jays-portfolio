b9f9da12a526517422075d4ba6f7d2648b6e0268
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-11 12:28:59 -0500

    using react icons package and changed contact links to svg

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,13 +1,16 @@
 import './contact.css'
+import {FaGithub} from 'react-icons/fa'
+import {FaLinkedin} from 'react-icons/fa'
+
 
 function Contact(){
     return (
         <section className='contactSection' id='contact'>
             <h1>Contact Me</h1>    
-            <p>Click one of the links to get in touch!</p>
+            <p>Can't wait to hear from you!</p>
             <div className='contactsContainer'>
-                <a href='https://www.linkedin.com/in/jahnoel-rondon/' target="_blank" rel="noreferrer">Linkedin</a>
-                <a href='https://github.com/JahnoelRondon' target="_blank" rel="noreferrer">GitHub</a>
+                <a href='https://www.linkedin.com/in/jahnoel-rondon/' target="_blank" rel="noreferrer"><FaLinkedin /></a>
+                <a href='https://github.com/JahnoelRondon' target="_blank" rel="noreferrer"><FaGithub /></a>
             </div>
         </section>
         
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,7 +7,7 @@ function Projects(){
         {
             title: 'Marq', 
             desc: 'Marq is a organizational/list-making application. It has many features to keep track of your time management as well as planning. There are many todo apps however Marq tackles the list making apps from a different angle and seperates the different kind of simple notes a person might make. With Marq you have the utilities right out of the box however you still have the flexibility to choose what you do and how you organize yourself.',
-            tech: "Django, Python, Bootstrap, Javascript, HTML and CSS",
+            tech: "Django, Python, PostgreSQL, Bootstrap, Javascript, HTML and CSS",
             img:'https://i.imgur.com/ryY4RLb.png',
             deployLink: 'https://p4marq.herokuapp.com/',
             ghubLink: 'https://github.com/JahnoelRondon/Marq-1'
