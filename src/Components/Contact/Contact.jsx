d7fef1a63d99aaa9dd47258ea5c67308c99425e6
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-13 19:05:02 -0500

    made form for contacting me

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,6 +12,15 @@ function Contact(){
                 <a href='https://www.linkedin.com/in/jahnoel-rondon/' target="_blank" rel="noreferrer"><FaLinkedin /></a>
                 <a href='https://github.com/JahnoelRondon' target="_blank" rel="noreferrer"><FaGithub /></a>
             </div>
+
+            <p>Email me</p>
+
+            <form action="https://formsubmit.co/jahnoelrondon03@gmail.com" method="POST" autocomplete="off">
+                <input type="email" name="email" placeholder='email' required/>
+                <input type="text" name="message" placeholder='write your message here' required/>
+                <button type="submit">Send</button>
+            </form>
+
         </section>
         
     )
