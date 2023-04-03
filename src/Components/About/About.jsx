49e98b0a6e24a55a9873e9ff46deb35b134a6cbb
Jahnoel Rondon <jahnoelrondon03@gmail.com>
2023-04-03 12:09:27 -0400

    restyled the about me sections to be column centered and with new avatar photo

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,4 +1,5 @@
 import './about.css'
+import pfp from "../../assets/images/pfp.PNG"
 import {FaGithub} from 'react-icons/fa'
 import {FaLinkedin} from 'react-icons/fa'
 
@@ -6,14 +7,15 @@ function About(){
     return(
         <section className='aboutContainer' id='about'>
             <div className='aboutFlex'>
-                <img src="https://i.imgur.com/PEUJU52.jpg" alt="pfp" width='260px' height='300px'/>
+                <img src={pfp} alt="pfp"/>
                 <div className='textContainer'>
                     <h1 className='about'>ABOUT ME</h1>
                     <p>Hello World! I am a Creative and visually inspired developer who is driven to bring ideas to life. I bring a curious and tenacious mindset when taking on new challenges. My past experience as a retention specialist at a staffing agency for unemployed job seekers during covid helped me manage stressful situations and unrealistic expectations. I constantly find out of the box solutions that create interactive and seamless design centered applications that delight users and clients alike.</p>
 
                     <div className="aboutLinks">
+                    <a className="joblinks" href='https://www.linkedin.com/in/jahnoel-rondon/' target="_blank" rel="noreferrer"><FaLinkedin /></a>
+
                         <a className='resume' href="https://docs.google.com/document/d/18JU7FdDmWty8jtxZUm73Lq5ynXq_p0A-nRLmhvx3qho/edit?usp=sharing" target="_blank" rel="noreferrer">View Resume</a>
-                        <a className="joblinks" href='https://www.linkedin.com/in/jahnoel-rondon/' target="_blank" rel="noreferrer"><FaLinkedin /></a>
                         <a className="joblinks" href='https://github.com/JahnoelRondon' target="_blank" rel="noreferrer"><FaGithub /></a>
                     </div>
 
