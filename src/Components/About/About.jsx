cb72da6867b86d3e1fc8bb5f535c60f2eba36354
Jahnoel Rondon <jahnoelrondon03@gmail.com>
2023-05-16 17:07:13 -0400

    edited skill section and about me section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,7 +10,7 @@ function About(){
                 <img src={pfp} alt="pfp"/>
                 <div className='textContainer'>
                     <h1 className='about'>ABOUT ME</h1>
-                    <p>Hello World! I am a Creative and visually inspired developer who is driven to bring ideas to life. I bring a curious and tenacious mindset when taking on new challenges. My past experience as a retention specialist at a staffing agency for unemployed job seekers during covid helped me manage stressful situations and unrealistic expectations. I constantly find out of the box solutions that create interactive and seamless design centered applications that delight users and clients alike.</p>
+                    <p>👋 <strong style={{color: "#f44336"}}>Hello World!</strong> I am a Creative and visually inspired developer who is driven to build my ideas to life. I have 1+ years of experience working in a team for small to large government projects. Before my developing journey I was a retention specialist at a staffing agency helping unemployed job seekers during covid and learned how to manage stressful situations and unrealistic expectations. I constantly find out of the box solutions that create interactive and seamless design centered applications that delight users and clients alike. When I am not developing you can find me on the piano, playing video games, or working out. Which means you wont find me at all because all be doing all of this in my house. 😅</p>
 
                     <div className="aboutLinks">
                     <a className="joblinks" href='https://www.linkedin.com/in/jahnoel-rondon/' target="_blank" rel="noreferrer"><FaLinkedin /></a>
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,49 +4,46 @@ import { FaTools } from "react-icons/fa";
 import { ImPaintFormat } from "react-icons/im";
 
 const SkillInfoData = [
-  {
-    title: "Frontend",
-    summary:
-      "I love to wonder, and enjoy bringing my imaginations and ideas to life on the screen.",
-    tools:
-      "React ~ Material UI ~ CSS3 ~ Javascript ~ DOM Manipulation ~ Responsive Design ~ HTML ~ Flex Box/Grid ~ Bootstrap ~ WireFraming",
-    icon: <ImPaintFormat />,
-  },
-  {
-    title: "Backend",
-    summary:
-      "I value the ability that allows the manipulation of data as it moves back and forth between the user and the server. ",
-    tools:
-      "MongoDB ~ NodeJS ~ Express ~ Mongoose ~ Django ~ Python ~ RESTful API ~ Fetch ~ SQL ~ PostgreSQL ~ MySQL",
-    icon: <FaServer />,
-  },
-  {
-    title: "Development",
-    summary:
-      "I genuinely enjoy expanding my creativity when it comes to developing not only the web, but creating/editing videos and video games.",
-    tools:
-      "Github ~ Git ~ Heroku ~ Netlify ~ Railway ~ Appian ~ C# ~ Unity Game Engine",
-    icon: <FaTools />,
-  },
+    {
+        title: "Frontend",
+        summary:
+            "I love to wonder, and enjoy bringing my imaginations and ideas to life on the screen.",
+        tools: "React ~ Material UI ~ CSS3 ~ Javascript ~ Responsive Design ~ HTML ~ Flex Box/Grid ~ Bootstrap ~ WireFraming",
+        icon: <ImPaintFormat />,
+    },
+    {
+        title: "Backend",
+        summary:
+            "I value the ability that allows the manipulation of data as it moves back and forth between the user and the server. ",
+        tools: "MongoDB ~ Mongoose ~ Django ~ Python ~ SQL ~ Sequelize ~ PostgreSQL ~ MySQL ~ Express.js ~ Node.js ~ RESTful API",
+        icon: <FaServer />,
+    },
+    {
+        title: "Development",
+        summary:
+            "I genuinely enjoy expanding my creativity when it comes to developing not only the web, but creating/editing videos and video games.",
+        tools: "Github ~ Git ~ AWS S3 ~ Heroku ~ Netlify ~ Railway ~ Appian ~ C# ~ Unity",
+        icon: <FaTools />,
+    },
 ];
 
 function Skills() {
-  return (
-    <section className="skillSection" id="skills">
-      <div className="skillsContainer">
-        {SkillInfoData &&
-          SkillInfoData.map((skill) => (
-            <div className="skillCard">
-              <div className="skillIcon">{skill.icon}</div>
-              <h4>{skill.title}</h4>
-              <p>{skill.summary}</p>
-              <p className="skillInfo">Tools:</p>
-              <p>{skill.tools}</p>
+    return (
+        <section className="skillSection" id="skills">
+            <div className="skillsContainer">
+                {SkillInfoData &&
+                    SkillInfoData.map((skill) => (
+                        <div className="skillCard">
+                            <div className="skillIcon">{skill.icon}</div>
+                            <h4>{skill.title}</h4>
+                            <p>{skill.summary}</p>
+                            <p className="skillInfo">Tools:</p>
+                            <p>{skill.tools}</p>
+                        </div>
+                    ))}
             </div>
-          ))}
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 export default Skills;
