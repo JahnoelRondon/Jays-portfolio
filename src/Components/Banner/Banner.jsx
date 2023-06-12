77601460104763997f5c2228172428e990512b41
Jahnoel Rondon <jahnoelrondon03@gmail.com>
2023-06-12 13:49:47 -0400

    added saturated background on top of banner image

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,13 +1,26 @@
-import './banner.css'
+import "./banner.css";
 
-function Banner(){
-    return (
-        <section className='bannerContainer'>
-            <h1>Jahnoel Rondon</h1>    
-            <p className='position'>Full Stack Developer</p>
-            <p className='badge'>Open to work!</p>
-        </section>
-    )
+function Banner() {
+  return (
+    <section className="bannerContainer">
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          /* items */
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundColor: "#00000032",
+        }}
+      >
+        <h1>Jahnoel Rondon</h1>
+        <p className="position">Full Stack Developer</p>
+        <p className="badge">Open to work!</p>
+      </div>
+    </section>
+  );
 }
 
-export default Banner
+export default Banner;
